1e304b4cc025a57d5d43015ad3e6c9fd2fee935a
Edgar Martinez <emartinez@dnanexus.com>
2022-06-25 11:29:32 +0000

    Continued working on the build script.

--- projects/grafana/auto/build.sh
+++ projects/grafana/auto/build.sh
@@ -1,16 +1,20 @@
 environmentName="${1:-dev}"
 scriptPath=`dirname $BASH_SOURCE`
 
-grafonnetLibLocation="$scriptPath/../src/grafonnet-lib"
+srcPath=`realpath $scriptPath/../src/`
+grafonnetLibLocation="$srcPath/grafonnet-lib"
 if [ ! -d $grafonnetLibLocation ]
 then
     echo "Cloning grafonnet-lib..."
     beginningLocation=`pwd`
-    cd $scriptPath/../src/
+    cd $srcPath
     git clone https://github.com/grafana/grafonnet-lib.git --quiet
     cd grafonnet-lib
     git checkout 6db00c292d3a1c71661fc875f90e0ec7caa538c2 --quiet
     cd $beginningLocation
 fi
 
-sleep 3 # simulates a build step
+for sourceFile in `ls $srcPath | grep '.*\.jsonnet'`
+do
+    echo "Compiling $sourceFile"
+done

d33fbf26d0ae7fb907a472d70f0830fdfb108caa
Edgar Martinez <emartinez@dnanexus.com>
2022-06-25 07:18:23 +0000

    Changed the ports that all of the containers are bound to so that theyre all 9090+.

--- projects/grafana/auto/release.sh
+++ projects/grafana/auto/release.sh
@@ -1,2 +1,2 @@
 environmentName="${1:-dev}"
-destination="${2:-http://host.docker.internal:3000}" # make sure to keep this default value in sync with docker-compose.yml
+destination="${2:-http://host.docker.internal:9092}" # make sure to keep this default value in sync with docker-compose.yml

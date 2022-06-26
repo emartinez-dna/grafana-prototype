1491f1bddff89d7de75ddec47ee9f282b69db948
Edgar Martinez <emartinez@dnanexus.com>
2022-06-26 02:43:28 +0000

    The whole dev workflow is now functioning as expected!

--- projects/grafana/auto/release.sh
+++ projects/grafana/auto/release.sh
@@ -1,2 +1,25 @@
 environmentName="${1:-dev}"
-destination="${2:-http://host.docker.internal:9092}" # make sure to keep this default value in sync with docker-compose.yml
+destination="${2:-host.docker.internal:9092}" # make sure to keep this default value in sync with docker-compose.yml
+username="${3:-admin}"
+password="${4:-admin}"
+protocol="${5:-http}"
+
+scriptPath=`dirname $BASH_SOURCE`
+distPath=`realpath $scriptPath/../dist`
+
+pushd $distPath
+
+for jsonFile in `ls $distPath | grep '.*\.json'`
+do
+    echo "POSTing $jsonFile to $destination"
+    payload="{\"dashboard\": $(jq . $jsonFile), \"overwrite\": true}"
+
+    fullUrl="$protocol://$username:$password@$destination/api/dashboards/db"
+
+    curl -X POST \
+        -H 'Content-Type: application/json' \
+        -d "${payload}" \
+       $fullUrl
+done
+
+popd

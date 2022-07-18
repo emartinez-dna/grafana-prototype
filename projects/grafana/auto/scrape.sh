5a69181271d22d28880c56779cc74cc18c80f318
Edgar Martinez <emartinez@dnanexus.com>
2022-07-18 06:24:23 +0000

    Implemented scraping from a target server, and saved the source code from vdev into this repo.

--- projects/grafana/auto/scrape.sh
+++ projects/grafana/auto/scrape.sh
@@ -3,8 +3,32 @@ destination="${2:-host.docker.internal:9092}" # make sure to keep this default v
 username="${3:-admin}"
 password="${4:-admin}"
 protocol="${5:-http}"
+out_dir=$6
+
+if [ -z $out_dir ]
+then
+    echo "out_dir is required."
+    exit 1
+else
+    out_dir=`realpath $out_dir`
+    mkdir -p $out_dir
+fi
 
 scriptPath=`dirname $BASH_SOURCE`
 source "$scriptPath/core.sh"
 
-restCommand $username $password $protocol $destination '' 'api/search?query=%' 'GET'
+uids=`restCommand $username $password $protocol $destination '' 'api/search?query=%' 'GET' | jq '.[].uid' --raw-output`
+declare -a parallelized_commands=()
+
+for uid in $uids
+do
+    # for every command, import core.sh and execute the restCommand for pulling the dashboard json by uid.
+    parallelized_commands+=("source $scriptPath/core.sh; restCommand '$username' '$password' '$protocol' '$destination' '' 'api/dashboards/uid/$uid' 'GET' | jq > '$out_dir/$uid.json'")
+done
+
+num_cores=`nproc --all`
+parallelism=$(( $num_cores * 4 ))
+echo "Pulling ${#parallelized_commands[@]} dashboards from $destination with parallelism of $parallelism and placing them in $out_dir."
+execution_time=`(time printf '%s\n' "${parallelized_commands[@]}" | parallel --jobs $parallelism bash -c) 2>&1 | awk '/^real/{print $2}'`
+
+echo "Done!  Operation took $execution_time."
